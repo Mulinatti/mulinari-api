546697efefd9ede66c386c4d02dad2db6f6842cc
mulinatti <gabrielmulinari2002@gmail.com>
2024-06-01 02:33:57 -0300

    fix: servico dados validation

--- src/main/java/mulinari/api/model/record/ServicoDados.java
+++ src/main/java/mulinari/api/model/record/ServicoDados.java
@@ -1,6 +1,8 @@
 package mulinari.api.model.record;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import mulinari.api.model.entity.Ajudante;
 
 import java.util.List;
@@ -9,8 +11,10 @@ public record ServicoDados(
 
         List<Ajudante> ajudantes,
 
+        @NotEmpty
         List<Long> ajudantesIds,
 
+        @NotNull
         Float valor,
 
         @NotBlank
